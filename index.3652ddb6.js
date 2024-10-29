4432d2a9fed02cde61d82d8d19396393e5e727e9
Łukasz <1luki9901@gmail.com>
2024-10-29 22:26:32 +0100

    make build

---
+++ index.3652ddb6.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){var t=document.querySelector(".wall"),e=document.querySelector(".spider");if(t&&e){var n=t.getBoundingClientRect(),o=e.getBoundingClientRect(),i=n.left+n.width/2-o.width/2,c=n.top+n.height/2-o.height/2;e.style.position="absolute",e.style.left="".concat(i,"px"),e.style.top="".concat(c,"px")}});
+//# sourceMappingURL=index.3652ddb6.js.map
